47e2e97905b3258ab4e8f19d59c5ff675bf53bae
Kiehyun Kevin Park <guitar79@gs.hs.kr>
2022-04-24 19:47:31 +0900

    update

--- 01-3_fits_astap_solving-listall.py
+++ 01-3_fits_astap_solving-listall.py
@@ -3,16 +3,14 @@
 Created on Thu Nov 22 01:00:19 2018
 @author: user
 
-No module named 'ccdproc'
-conda install -c conda-forge ccdproc
 """
 from glob import glob
 import os
 import subprocess
 from datetime import datetime
 from astropy.io import fits
+import shutil 
 import Python_utilities
-import astro_utilities
 
 log_dir = "logs/"
 log_file = "{}{}.log".format(log_dir, os.path.basename(__file__)[:-3])
@@ -22,19 +20,20 @@ print ("err_log_file: {}".format(err_log_file))
 
 base_dir = "../CCD_new_files/"
 #base_dir = "../CCD_wcs_one/"
-base_dir = "../CCD_obs_raw/QSI683ws_1bin/Light_FSQ106ED-x73/"
-
+#base_dir = "../CCD_obs_raw/"
 
 fullnames = Python_utilities.getFullnameListOfallFiles(base_dir)
 print ("fullnames: {}".format(fullnames))
 
+fullnames_fit = [w for w in fullnames if ".fit" in w]
+
 n = 0
-for fullname in fullnames[:] :
+for fullname in fullnames_fit[:] :
 #fullname = fullnames[5]
     n += 1
     print('#'*40,
-        "\n{2:.01f}%  ({0}/{1}) {3}".format(n, len(fullnames), (n/len(fullnames))*100, os.path.basename(__file__)))
-    print ("Starting...   fullname: {}".format(fullname))
+        "\n{2:.01f}%  ({0}/{1}) {3}".format(n, len(fullnames), (n/len(fullnames_fit))*100, os.path.basename(__file__)))
+    print ("Starting...\nfullname: {}".format(fullname))
 
     fullname_el = fullname.split("/")
     filename_el = fullname_el[-1].split("_")
@@ -45,7 +44,9 @@ for fullname in fullnames[:] :
                 print("Trying image sovser using ASTAP...")
 
             else : 
+                
                 hdul = fits.open(fullname)
+                print("fits file is opened".format(fullname_el[-1]))
 
                 if "light" in hdul[0].header["IMAGETYP"].lower() :
                     print("{} is light frame".format(fullname_el[-1]))
@@ -54,7 +55,7 @@ for fullname in fullnames[:] :
                         with subprocess.Popen(['astap', 
                                     '-f', 
                                     '{0}'.format(fullname), 
-                                    #'-update'
+                                    '-update'
                                     '-analyse2'
                                     ],
                                     stdout=subprocess.PIPE) as proc :
@@ -71,3 +72,26 @@ for fullname in fullnames[:] :
         except Exception as err:
             Python_utilities.write_log(err_log_file,
                     '{2} ::: {0} with solve {1} '.format(err, fullname, datetime.now()))
+
+#####################################################################
+fullnames = Python_utilities.getFullnameListOfallFiles(base_dir)
+print ("fullnames: {}".format(fullnames))
+
+fullnames_tmp = [w for w in fullnames if ".tmp" in w]
+
+n = 0
+for fullname in fullnames_tmp[:] :
+#fullname = fullnames[5]
+    n += 1
+    print('#'*40,
+        "\n{2:.01f}%  ({0}/{1}) {3}".format(n, len(fullnames), (n/len(fullnames_tmp))*100, os.path.basename(__file__)))
+    print ("Starting...\nfullname: {}".format(fullname))
+
+    try:
+        shutil.move(r"{}".format(fullname), \
+                        r"{}.fit".format(fullname[:-4]))
+
+    except Exception as err:
+        Python_utilities.write_log(err_log_file,
+                    '{2} ::: {0} with solve {1} '.format(err, fullname, datetime.now()))
+        
