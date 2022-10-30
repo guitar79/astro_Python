6bbe3b2c596c43e4f881922f27ce10f9b3d3bea3
Kiehyun Kevin Park <guitar79@gs.hs.kr>
2022-10-30 15:43:27 +0900

    update

--- _01-2_3.sh
+++ _01-2_3.sh
@@ -2,7 +2,7 @@
 #conda activate astro_Python_ubuntu_env
 #cd /mnt/Rdata/CCD_obs/astro_Python
 #python 01-1_modify_fits_header_listall.py
-python 01-2_fits_solving-listall.py
+python 01-2_fits_solving-listall_MP.py
 python 01-3_move_fits_files_with_optic_keyword_listall.py
 #python 01-4_rename_wcs_fits_files_with_optic_keyword_listall.py
 
