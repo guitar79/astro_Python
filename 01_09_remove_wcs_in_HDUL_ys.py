96e18b7065d9ce526efde5ed09db9fd4a51af9e6
Kiehyun Kevin Park <guitar79@gs.hs.kr>
2023-02-22 19:56:50 +0900

    Add files via upload

--- 01_09_remove_wcs_in_HDUL_ys.py
+++ 01_09_remove_wcs_in_HDUL_ys.py
@@ -41,60 +41,62 @@ if not os.path.exists('{0}'.format(log_dir)):
 #%%
 #######################################################
 # read all files in base directory for processing
-BASEDIR = astro_utilities.base_dir
+BASEDIR = Path("/mnt/OBS_data") 
+DOINGDIR = Path( BASEDIR/ astro_utilities.CCD_obs_raw_dir)
+
+DOINGDIRs = sorted(Python_utilities.getFullnameListOfallsubDirs(DOINGDIR))
+#print ("DOINGDIRs: ", format(DOINGDIRs))
+print ("len(DOINGDIRs): ", format(len(DOINGDIRs)))
 
 #%%
-BASEDIRs = sorted(Python_utilities.getFullnameListOfsubDir(BASEDIR))
-print ("BASEDIRs: {}".format(BASEDIRs))
-
-#%%
-for BASEDIR in BASEDIRs :
+for DOINGDIR in DOINGDIRs :
     # 디렉토리 하나씩 loop...
-    print ("Starting...\n{}".format(BASEDIR))
+    print ("Starting...\n{}".format(DOINGDIR))
 
-    BASEDIR = Path(BASEDIR)
+    DOINGDIR = Path(DOINGDIR)
     
-    RESULTDIR = BASEDIR / astro_utilities.DAOfinder_result_dir
-    SOLVEDDIR = BASEDIR / astro_utilities.solved_dir
-    MASTERDIR = BASEDIR / astro_utilities.master_dir
-    REDUCEDDIR = BASEDIR / astro_utilities.reduced_dir
-    MASTERDIR = BASEDIR / astro_utilities.master_dir
+    RESULTDIR = DOINGDIR / astro_utilities.DAOfinder_result_dir
+    SOLVEDDIR = DOINGDIR / astro_utilities.solved_dir
+    MASTERDIR = DOINGDIR / astro_utilities.master_dir
+    REDUCEDDIR = DOINGDIR / astro_utilities.reduced_dir
+    MASTERDIR = DOINGDIR / astro_utilities.master_dir
 
     #try : 
     #파일 목록을 DataFrame으로 만들어서 이용하자
     #summary = yfu.make_summary(REDUCEDDIR / "*.fit*")
-    summary = yfu.make_summary(BASEDIR / "*.fit*")
+    summary = yfu.make_summary(DOINGDIR / "*.fit*")
                         #keywords = ["DATE-OBS", "FILTER", "OBJECT", "IMAGETYP"],  # header keywords; actually it is case-insensitive
                         #fname_option = 'name',  # 'file' column will contain only the name of the file (not full path)
-                        #output = "{}summary.csv".format(BASEDIR),
+                        #output = "{}summary.csv".format(DOINGDIR),
                         #sort_by = "DATE-OBS"  # 'file' column will be sorted based on "DATE-OBS" value in the header
                         #)
     print("summary:\n {}".format(summary))
 
-    # light frame  만 선택
-    df_light = summary[summary["IMAGETYP"] == "LIGHT"]
-    #print ("df_light: {}".format(df_light))
-    #print ("len(df_light): {}".format(len(df_light)))
+    try:
+        # light frame  만 선택
+        df_light = summary[summary["IMAGETYP"] == "LIGHT"]
+        #print ("df_light: {}".format(df_light))
+        #print ("len(df_light): {}".format(len(df_light)))
 
-    for _, row in df_light.iterrows():
-        # 파일명 출력
-        print (row["file"])
-        fpath = Path(row["file"])
-        new_fpath = Path(f"{fpath.parents[0]}/{fpath.stem}_clean.fit")
-        # fits hedaer 에 있는 wcs 정보를 지운다
-        yfu.wcsremove(fpath, 
-                    additional_keys=["COMMENT"],
-                    verbose=True,
-                    output=new_fpath,
-                    ccddata=True,
-                    overwrite=True)
-        if new_fpath.exists() \
-            and fpath.exists():
-            print("rename", f"{str(new_fpath)}", f"{str(fpath)}")
-            #os.rename(f"{str(new_fpath)}", f"{str(fpath)}")
-            shutil.move(f"{str(new_fpath)}", f"{str(fpath)}")
-            
-    # except Exception as err :
-    #     print("X"*60)
-    #     print('{0}'.format(err))
+        for _, row in df_light.iterrows():
+            # 파일명 출력
+            print (row["file"])
+            fpath = Path(row["file"])
+            new_fpath = Path(f"{fpath.parents[0]}/{fpath.stem}_clean.fit")
+            # fits hedaer 에 있는 wcs 정보를 지운다
+            yfu.wcsremove(fpath, 
+                        additional_keys=["COMMENT"],
+                        verbose=True,
+                        output=new_fpath,
+                        ccddata=True,
+                        overwrite=True)
+            if new_fpath.exists() \
+                and fpath.exists():
+                print("rename", f"{str(new_fpath)}", f"{str(fpath)}")
+                #os.rename(f"{str(new_fpath)}", f"{str(fpath)}")
+                shutil.move(f"{str(new_fpath)}", f"{str(fpath)}")
+                
+    except Exception as err :
+        print("X"*60)
+        print('{0}'.format(err))
 
