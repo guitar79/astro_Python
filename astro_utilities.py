1b6a7974c4823898f7f936fcd31bf51729e6688d
Kiehyun Kevin Park <guitar79@gs.hs.kr>
2023-02-23 13:06:57 +0900

    update

--- astro_utilities.py
+++ astro_utilities.py
@@ -140,9 +140,131 @@ def calPixScale (F_length, Opt_acc, Pix_size) :
     PIXScale = Pix_size / (F_length * Opt_acc) *  206.265
     return PIXScale
 
+#%%
+def KevinFitsUpdater(fpath):
+    fpath = Path(fpath)
+
+    foldername_el = fpath.parts[-2].split('_')
+    fname_el = fpath.parts[-1].split('_')
+    print("foldername_el", foldername_el)
+    print("fname_el", fname_el)
+    object_name = foldername_el[0]
+    filter_name = fname_el[2]
+    optic_name = foldername_el[5]
+    ccd_name = foldername_el[6]
+    print("object_name", object_name)
+    print("filter_name", filter_name)
+    print("optic_name", optic_name)
+    print("ccd_name", ccd_name)
+
+     
+    with fits.open(str(fpath), mode="append") as hdul :
+        for checkKEY in checkKEYs: 
+            if not checkKEY in hdul[0].header :
+                hdul[0].header.append(checkKEY, 
+                                '', 
+                                f"The keyword '{checkKEY}' is added.") 
+            print(f"{checkKEY}: ", hdul[0].header[checkKEY])
+
+        hdul.flush()  # changes are written back to original.fits
+
+    # Change something in hdul.
+    with fits.open(str(fpath), mode="update") as hdul :
+        
+        #if object_name != hdul[0].header["OBJECT"] : 
+        hdul[0].header["OBJECT"] = object_name.upper()
+        print(f"The 'OBJECT' is set {object_name.upper()}")
+    
+        if len(hdul[0].header['DATE-OBS']) == 10 \
+            and 'TIME-OBS' in hdul[0].header : 
+            hdul[0].header['DATE-OBS'] += 'T' + hdul[0].header['TIME-OBS']
+            print(f"The 'DATE-OBS' is set {hdul[0].header['DATE-OBS']}")
+
+        if "ze" in hdul[0].header["IMAGETYP"].lower() \
+                or "bi" in hdul[0].header["IMAGETYP"].lower() :
+            hdul[0].header["IMAGETYP"] = "BIAS"
+            print(f"The 'IMAGETYP' is set {hdul[0].header['IMAGETYP']}")
+        elif "da" in hdul[0].header["IMAGETYP"].lower() :
+            hdul[0].header["IMAGETYP"] = "DARK"
+            print(f"The 'IMAGETYP' is set {hdul[0].header['IMAGETYP']}")
+        elif "fl" in hdul[0].header["IMAGETYP"].lower() :
+            hdul[0].header["IMAGETYP"] = "FLAT"
+            print(f"The 'IMAGETYP' is set {hdul[0].header['IMAGETYP']}")
+        elif "da" in hdul[0].header["IMAGETYP"].lower() \
+                or "lig" in hdul[0].header["IMAGETYP"].lower() :
+            hdul[0].header["IMAGETYP"] = "LIGHT"
+            print(f"The 'IMAGETYP' is set {hdul[0].header['IMAGETYP']}")
+        
+        if "BIAS" in hdul[0].header["IMAGETYP"] \
+            or "DARK" in hdul[0].header["IMAGETYP"] :
+            hdul[0].header["FILTER"] = "-"
+            print(f"The 'FILTER' is set {hdul[0].header['FILTER']}")
+            hdul[0].header['OPTIC'] = "-"
+            print(f"The 'OPTIC' is set {hdul[0].header['OPTIC']}")
+
+        if "FLAT" in hdul[0].header["IMAGETYP"] \
+            or "LIGHT" in hdul[0].header["IMAGETYP"] :
+            hdul[0].header["FILTER"] = filter_name.upper()
+            print(f"The 'FILTER' is set {hdul[0].header['FILTER']}")
+            
+            hdul[0].header["OPTIC"] = optic_name
+            print(f"The 'OPTIC' is set {hdul[0].header['OPTIC']}")
+
+        try : 
+            if 'qsi' in hdul[0].header['INSTRUME'] :     
+                CCDNAME = 'QSI683ws'
+            elif 'st-8300' in hdul[0].header['INSTRUME'] : 
+                CCDNAME = 'ST-8300M'
+            elif 'stf-8300' in hdul[0].header['INSTRUME'] : 
+                CCDNAME = 'STF-8300M'
+            elif '11000' in hdul[0].header['INSTRUME'] : 
+                CCDNAME = 'STL-11000M'
+            elif '16803' in hdul[0].header['INSTRUME'] : 
+                CCDNAME = 'STX-16803'
+            elif "SBIG" in hdul[0].header['INSTRUME'] :
+                if hdul[0].header['XPIXSZ'] == 5.4 \
+                        or hdul[0].header['XPIXSZ'] == 10.8 :
+                    CCDNAME = 'STF-8300M'
+                elif hdul[0].header['XPIXSZ'] == 9.0 \
+                        or hdul[0].header['XPIXSZ'] == 18.0 :
+                    if hdul[0].header['NAXIS1'] == 2048 \
+                            or  hdul[0].header['NAXIS1'] == 4096 :
+                        CCDNAME = 'STX-16803'
+                    elif hdul[0].header['NAXIS1'] == 4008 \
+                            or  hdul[0].header['NAXIS1'] == 2672 \
+                            or  hdul[0].header['NAXIS1'] == 2004 \
+                            or  hdul[0].header['NAXIS1'] == 1336 :
+                        CCDNAME = 'STL-11000M'
+            else :
+                CCDNAME = ccd_name    
+        except :
+            CCDNAME = ccd_name
+        print("CCDNAME", CCDNAME)
+
+        hdul[0].header["CCDNAME"] = CCDNAME
+        print(f"The 'CCDNAME' is set {CCDNAME}...")
+
+        if not "CCD-TEMP" in hdul[0].header :
+            hdul[0].header['CCD-TEMP'] = 'N'
+            print(f"The 'CCD-TEMP' is set {hdul[0].header['CCD-TEMP']}...")
+
+        if not "EXPOSURE" in hdul[0].header :
+            hdul[0].header["EXPOSURE"] = hdul[0].header["EXPTIME"]
+            print(f"The 'EXPOSURE' is set {hdul[0].header['EXPTIME']}...")
+
+        hdul[0].header['GAIN'] = GAINDIC[CCDNAME]
+        print(f"The 'GAIN' is set {hdul[0].header['GAIN']}...")
+        hdul[0].header['EGAIN'] = GAINDIC[CCDNAME]
+        print(f"The 'EGAIN' is set {hdul[0].header['EGAIN']}...")
+        hdul[0].header['RDNOISE'] = RDNOISEDIC[CCDNAME]
+        print(f"The 'RDNOISE' is set {hdul[0].header['RDNOISE']}...")
+        hdul.flush()  # changes are written back to original.fits
+        print('*'*30)
+        print(f"The header of {fpath.name} is updated..")
+    return hdul
 
 #%%
-class KevinFitsHeader():
+""" class KevinFitsHeader():
     def __init__(self, fpath):
         self.fpath = Path(fpath)
         self.checkKEYs = ["OBJECT", "TELESCOP", "OPTIC", "CCDNAME", 'FILTER',
@@ -274,7 +396,7 @@ class KevinFitsHeader():
             print('*'*30)
             print(f"The header of {self.fpath.name} is updated..")
         return self.hdul
-
+ """
 #%%
 #########################################
 #single  KevinPSolver
